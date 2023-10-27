29eab049bb2a99a032319a66217017879b5a1e0f
borbaaxz <gabrielborbaaxz@gmail.com>
2023-10-26 23:15:51 -0300

    adicionando um novo arquivo

---
+++ animals.js
@@ -0,0 +1,7 @@
+const animals = ["gato", "arara", "lebre", "aguia", "tubarao", "leão"];
+if(!animals.includes("leão")){
+    console.log("Leão não existe no array animals.");
+}
+else{
+    console.log("Existe um leão no array animals.");
+}
---
+++ series.js
@@ -0,0 +1,5 @@
+const my3FavoriteTVShows = ["The boy", "Prison Break", "Breaking Bad"];
+console.log(my3FavoriteTVShows);
+
+const itens = my3FavoriteTVShows.length === 3;
+console.log(itens);
